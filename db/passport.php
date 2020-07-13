4f35cad1e3d0723c680f04255409cc6ebca0061b
hariom940 <rhariom@deaninfotech.com>
2020-07-13 17:06:19 +0530

    login

---
+++ db/passport.php
@@ -0,0 +1,58 @@
+ public function login(){ 
+            if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){ 
+                $user = Auth::user(); 
+                $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+                return response()->json(['success' => $success], $this-> successStatus); 
+            } 
+            else{ 
+                return response()->json(['error'=>'Unauthorised'], 401); 
+            } 
+        }
+    /** 
+         * Register api 
+         * 
+         * @return \Illuminate\Http\Response 
+         */ 
+        public function register(Request $request) 
+        { 
+            $validator = Validator::make($request->all(), [ 
+                'name' => 'required', 
+                'email' => 'required|email', 
+                'password' => 'required', 
+                'c_password' => 'required|same:password', 
+            ]);
+    if ($validator->fails()) { 
+                return response()->json(['error'=>$validator->errors()], 401);            
+            }
+    $input = $request->all(); 
+            $input['password'] = bcrypt($input['password']); 
+            $user = User::create($input); 
+            $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+            $success['name'] =  $user->name;
+    return response()->json(['success'=>$success], $this-> successStatus); 
+        }
+    /** 
+         * details api 
+         * 
+         * @return \Illuminate\Http\Response 
+         */ 
+        public function details() 
+        { 
+            $user = Auth::user(); 
+            return response()->json(['success' => $user], $this-> successStatus); 
+        } 
+
+
+
+
+
+
+
+        //=====Route =======//
+        Route::any('register', 'controller@register');
+Route::post('product','Products@save');
+
+Route::post('login', 'controller@login');
+Route::group(['middleware' => 'auth:api'], function(){
+Route::post('details', 'controller@details');
+});
