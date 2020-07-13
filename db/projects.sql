4745fc262fbe5a9d260a02fad3816d9d79c3d4c1
hariom940 <rhariom@deaninfotech.com>
2020-07-13 17:12:05 +0530

    login

--- db/projects.sql
+++ db/projects.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jul 08, 2020 at 10:48 PM
--- Server version: 5.7.29-0ubuntu0.18.04.1
--- PHP Version: 5.6.40-29+ubuntu18.04.1+deb.sury.org+1
+-- Host: localhost
+-- Generation Time: Jul 13, 2020 at 01:40 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -32,7 +34,7 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -61,7 +63,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (7, '2019_08_19_000000_create_failed_jobs_table', 1),
 (8, '2020_06_22_074823_student', 2),
 (9, '2020_06_22_075634_students', 3),
-(10, '2020_07_05_092206_create_tasks_table', 4);
+(10, '2020_07_05_092206_create_tasks_table', 4),
+(11, '2014_10_12_100000_create_password_resets_table', 5);
 
 -- --------------------------------------------------------
 
@@ -74,12 +77,22 @@ CREATE TABLE `oauth_access_tokens` (
   `user_id` bigint(20) UNSIGNED DEFAULT NULL,
   `client_id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `scopes` text COLLATE utf8mb4_unicode_ci,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `revoked` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `expires_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `oauth_access_tokens`
+--
+
+INSERT INTO `oauth_access_tokens` (`id`, `user_id`, `client_id`, `name`, `scopes`, `revoked`, `created_at`, `updated_at`, `expires_at`) VALUES
+('3a99ef7eefa96f58a96785ed0aa7c0c12b003889c89ef99a68046392afb7246548c29a68eac36c4b', 10, 3, 'MyApp', '[]', 0, '2020-07-13 02:45:21', '2020-07-13 02:45:21', '2021-07-13 08:15:21'),
+('52bd3ae17a395cb37e60aa098a72766823343c5b252272292547174af987ce1f4c840dc5def62f4e', 8, 3, 'MyApp', '[]', 0, '2020-07-13 02:07:55', '2020-07-13 02:07:55', '2021-07-13 07:37:55'),
+('bd970b2be22dbd7ffba94bbc99f7d897063fde6134187212dc0a44b272874235429719c39594196d', 11, 3, 'MyApp', '[]', 0, '2020-07-13 02:52:53', '2020-07-13 02:52:53', '2021-07-13 08:22:53'),
+('bf0ba23ceed0ad272938966a87b44e3e4b2c508550a4e1a0006d425226c2077f52cf802e7529ea00', 13, 3, 'MyApp', '[]', 0, '2020-07-13 02:56:51', '2020-07-13 02:56:51', '2021-07-13 08:26:51');
 
 -- --------------------------------------------------------
 
@@ -91,7 +104,7 @@ CREATE TABLE `oauth_auth_codes` (
   `id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `client_id` bigint(20) UNSIGNED NOT NULL,
-  `scopes` text COLLATE utf8mb4_unicode_ci,
+  `scopes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `revoked` tinyint(1) NOT NULL,
   `expires_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -163,6 +176,18 @@ CREATE TABLE `oauth_refresh_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tasks`
 --
 
@@ -215,7 +240,8 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `user_password`
 (2, 'Har', 'g@gmail.com', NULL, '$2y$10$w2zFq4UJI16mAYntKPF0NOM458kvxog8muroUcWaFOosp1fqv/ZX6', 'USER', NULL, '2020-07-04 04:51:26', '2020-07-04 04:51:26'),
 (3, 'vishnu', 'vishnu@gmail.com', NULL, '$2y$10$aOUsaGl/mBizkQ79P0KudepWR0FQkXDJXC7MpSFm3hls5qe2D0ii.', 'USER', NULL, '2020-07-08 07:14:29', '2020-07-08 07:14:29'),
 (4, 'tyredekho', 'tyredekho@gmail.com', NULL, 'tyredekho', 'USER', NULL, '2020-07-08 08:39:31', '2020-07-08 08:39:31'),
-(5, 'hariom', 'hariom@gmail.com', NULL, '$2y$10$iP9delv4P9obhRyuuSU.UO1PciAn7V9kjQgmELGUaUVmsKxZRlTt6', 'USER', NULL, '2020-07-08 08:40:46', '2020-07-08 08:40:46');
+(5, 'hariom', 'hariom@gmail.com', NULL, '$2y$10$iP9delv4P9obhRyuuSU.UO1PciAn7V9kjQgmELGUaUVmsKxZRlTt6', 'USER', NULL, '2020-07-08 08:40:46', '2020-07-08 08:40:46'),
+(13, 'ram', 't@gmail.com', NULL, '$2y$10$7T7EF.mdo.WkoPUwot5vOuRBV9w7zke63K8IQXDfsP2ygFg1D4hM6', 'USER', NULL, '2020-07-13 02:56:49', '2020-07-13 02:56:49');
 
 --
 -- Indexes for dumped tables
@@ -267,6 +293,12 @@ ALTER TABLE `oauth_refresh_tokens`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD KEY `password_resets_email_index` (`email`);
+
+--
 -- Indexes for table `tasks`
 --
 ALTER TABLE `tasks`
@@ -288,31 +320,38 @@ ALTER TABLE `users`
 --
 ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
 --
 -- AUTO_INCREMENT for table `oauth_clients`
 --
 ALTER TABLE `oauth_clients`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT for table `oauth_personal_access_clients`
 --
 ALTER TABLE `oauth_personal_access_clients`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
